dbc85a7b0269d963d1282c5ffb2b86f7535709c1
menv <mennovermeulen82@live.nl>
2021-04-15 14:29:24 +0200

    ✅ basic input from user test

--- hangman/__init__.py
+++ hangman/__init__.py
@@ -28,16 +28,28 @@ class Configurations:
     min_length: int = MIN_LENGTH
     max_length: int = MAX_LENGTH
 
+@dataclass()
+class Guess:
+    """
+    The Guess contains a certain guess. This could be either one character 
+    or a guessed string.
+    """
+    guess: str
 
+@dataclass()
 class State:
     """
     The state of the system describes the to be guessed word,
     all the wrongly guessed characters, the current progress of the word,
     the number of Lives and the configuration options.
     """
+    target_word: str
+    current_lives: int
+    guesses: List[Guess]
 
-    def __init__(self, config: Configurations):
-        self.lives = config.lives
+
+# the global static object for state
+current_state: State = State(target_word="penguin",current_lives=1, guesses=[])
 
 
 def print_error(string: str):
@@ -110,3 +122,20 @@ def parse_args(argList: List[str]) -> Configurations:
         min_length=args.minimum_length,
         max_length=args.maximum_length
     )
+
+# Get user input from Stdin
+def get_guess() -> Guess:
+    user_input = input("Please enter your guess: ")
+    if len(user_input) == 1:
+        return Guess(guess=user_input)
+
+    if len(user_input) == 0:
+        return None
+    
+    if len(user_input) == len(current_state.target_word):
+        return Guess(guess=user_input)
+    error_msg = "the word to be guessed has a different length"
+    print_error(error_msg)
+    raise ValueError(error_msg)
+
+    
