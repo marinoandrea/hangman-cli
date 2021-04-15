5a032471d9020fde9bae6b3f97b6a36d2e4376d2
Andrea Marino <am.marinoandrea@gmail.com>
2021-04-15 21:30:35 +0200

    🚧 main loop init (work in progress)

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-from hangman.core import State
+import sys
+
+from hangman.core import State, update_game
+from hangman.io import get_guess, get_play_new_game, parse_args
 
 
 def main():
-    current_state = State(target_word="penguin", current_lives=1, guesses=[])
+    config = parse_args(sys.argv[1:])
 
-    while True:
-        pass
+    is_prog_running = True
+
+    while is_prog_running:
+        state = State.from_config(config)
+
+        try:
+            while state.is_running:
+                try:
+                    guess = get_guess(state)
+                    state.guesses.append(guess)
+                    update_game(state)
+                except KeyboardInterrupt:
+                    state.is_running = False
+                    # NOTE(andrea): this is just for aesthetic purposes
+                    print('\n')
+
+            is_prog_running = get_play_new_game()
+
+        except KeyboardInterrupt:
+            is_prog_running = False
+            # NOTE(andrea): this is just for aesthetic purposes
+            print('\n')
+
+    print('Thank your for playing :)')
 
 
 if __name__ == '__main__':
